1dc68293de5a7b7b9c900a3101c674b98276f1e7
jaypee15 <okoyejohnpaul15@gmail.com>
2023-08-07 19:41:36 +0100

    users now able to login and register

--- todo/views.py
+++ todo/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.urls import reverse_lazy
 from .models import Task
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
-class TaskDetail(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        return context
+
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
+
+    def get_queryset(self):
+        base_qs = super(TaskDetail, self).get_queryset()
+        return base_qs.filter(user=self.request.user)
     
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = ['title','description','completed']
     success_url = reverse_lazy('tasks')
@@ -25,6 +35,34 @@ class TaskCreate(CreateView):
         form.instance.user = self.request.user
         messages.success(self.request, "The task was created successfully.")
         return super(TaskCreate,self).form_valid(form)
+    
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = ['title','description','completed']
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        messages.success(self.request, "The task was updated successfully.")
+        return super(TaskUpdate,self).form_valid(form)
+    
+    def get_queryset(self):
+        base_qs = super(TaskDetail, self).get_queryset()
+        return base_qs.filter(user=self.request.user)
+    
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        messages.success(self.request, "The task was deleted successfully.")
+        return super(TaskDelete,self).form_valid(form)
+    
+    def get_queryset(self):
+        base_qs = super(TaskDetail, self).get_queryset()
+        return base_qs.filter(user=self.request.user)
 
 def home(request):
     return render(request,'home.html')
